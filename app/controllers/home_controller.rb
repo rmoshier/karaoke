779f063363fdff146576a604177ebefc5de4ff16
Rachel <rmoshier@gmail.com>
2015-01-28 11:02:42 -0800

    user model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     # Initialize a new Rdio client
-    @client = RdioApi.new(:consumer_key => RDIO_API_KEY, :consumer_secret => RDIO_API_SECRET)
+    # @client = RdioApi.new(:consumer_key => RDIO_API_KEY, :consumer_secret => RDIO_API_SECRET)
   end
 end

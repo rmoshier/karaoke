b838cd18e1029bd5f633109cddc42cb8e2c35b77
Rachel <rmoshier@gmail.com>
2015-01-29 15:16:28 -0800

    adding rdio get music logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    # Initialize a new Rdio client
-    # @client = RdioApi.new(:consumer_key => RDIO_API_KEY, :consumer_secret => RDIO_API_SECRET)
+    RdioSetter.new(session[:user_id])
   end
 end
---
+++ lib/rdio.rb
@@ -0,0 +1,24 @@
+require 'httparty'
+
+class RdioSetter
+
+  def create_rdio_client
+    # Initialize a new Rdio client
+    @client = RdioApi.new(:consumer_key => RDIO_API_KEY, :consumer_secret => RDIO_API_SECRET)
+    # raise
+  end
+
+  def heavy_rotation
+    # Get songs in heavy rotation
+    @client.getHeavyRotation(:type => "albums")
+  end
+
+end
+
+# Methods that act on behalf of a user require an access token, OmniAuth is best for this
+
+# Access token can be set at initialization
+# client = RdioApi.new(:consumer_key => CONSUMER_KEY,
+#                      :consumer_secret => CONSUMER_SECRET,
+#                      :access_token => ACCESS_TOKEN,
+#                      :access_secret => ACCESS_SECRET)

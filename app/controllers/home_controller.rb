cbe600e2ac8b062d5adf27067ccef296e06690ac
Rachel <rmoshier@gmail.com>
2015-01-30 09:05:57 -0800

    json object on screen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ require_dependency './lib/rdio'
 class HomeController < ApplicationController
   def index
     if current_user
-      RdioSetter.new(current_user)
-      @albums = heavy_rotation
+      @albums = RdioSetter.new(current_user).heavy_rotation
+      # @albums = heavy_rotation
       # @albums = RdioSetter.heavy_rotation
     end
     # raise

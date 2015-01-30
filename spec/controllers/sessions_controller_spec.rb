b6ef59e5b99f6ba9aee36566c206786031ec26e8
Rachel <rmoshier@gmail.com>
2015-01-30 15:18:17 -0800

    testing session controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do
 
+  describe SessionsController do
+    describe "#destroy" do
+      it "successfully logs out a user" do
+        expect(session[:uid]).to be_nil
+      end
+    end
+
+    describe "#destroy" do
+      it "successfully sends the correct HTTP response, redirect" do
+        get :destroy
+        expect(response.status).to eq 302
+      end
+    end
+
+    describe "#create" do
+      it "succesfully finds a pre-existing user in the database" do
+        user_id = 13
+        expect session[user_id] = 13
+      end
+    end
+  end
+
 end

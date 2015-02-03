dc4ceafb41f152fcfe51421c4873c370fe0d16b8
Rachel <rmoshier@gmail.com>
2015-02-03 14:55:46 -0800

    http party up and running

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 require_dependency './lib/rdio'
+require_dependency './lib/musixmatch'
 
 class HomeController < ApplicationController
   def index
     if current_user
       @playlists = RdioSetter.new(current_user).get_playlists
       @key = @playlists["owned"][0]["key"]
+      @json = MusixmatchSetter.new.response
     end
+    # raise
+    # @lyrics = MusixmatchSetter.new.lyrics_search
   end
 end
---
+++ lib/musixmatch.rb
@@ -0,0 +1,46 @@
+require 'httparty'
+require 'json'
+
+class MusixmatchSetter
+
+  # include HTTParty
+  # base_uri 'http://api.musixmatch.com/ws/1.1/'
+
+  # def initialize
+  #   response
+  # end
+
+  def response
+    # url = "https://api.flickr.com/services/rest/?&method=#{method}&api_key=#{ENV['FLICKR_KEY']}&#{id_type}=#{id}#{extras}"
+    url = "http://api.musixmatch.com/ws/1.1/track.search?apikey=1ded3ade3e63977aef9212b43320afb1&q_track=back%20to%20december&q_artist=taylor%20swift&f_has_lyrics=1"
+    # url = "http://api.musixmatch.com/ws/1.1/#{method}?apikey=#{ENV['MUSIXMATCH_API_KEY']}&q_track=back%20to%20december&q_artist=taylor%20swift&f_has_lyrics=1"
+    # response = HTTParty.get(url)
+    # @response = response.parsed_response
+    JSON.parse(HTTParty.get(url))
+  end
+
+  # when they press play load in artist and albumn name as variables into fat url
+  # take it in and run a method were for every space we throw in a '%'
+
+
+
+
+  # def initialize
+  #   create_musixmatch_client
+  #   lyrics_search
+  # end
+  #
+  # def create_musixmatch_client
+  #   @mmclient = MusixMatch::API::Base.api_key = ENV['MUSIXMATCH_API_KEY']
+  # end
+  #
+  # def lyrics_search
+  #   @response = MusixMatch.search_lyrics(:q_artist => 'Pantera')
+  #   if @response.status_code == 200
+  #     @response.each do |lyrics|
+  #       puts "#{lyrics.lyrics_id}: #{lyrics.track_name} (#{lyrics.artist_name})"
+  #     end
+  #   end
+  # end
+
+end

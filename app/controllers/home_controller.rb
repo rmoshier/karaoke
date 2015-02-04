3c61a3be9dcbae1c4f35921008961690f458e017
Rachel <rmoshier@gmail.com>
2015-02-04 10:43:28 -0800

    renaming musixmatch.rb method names

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,9 @@ class HomeController < ApplicationController
     if current_user
       @playlists = RdioSetter.new(current_user).get_playlists
       @key = @playlists["owned"][0]["key"]
-      @json = MusixmatchSetter.new.response
-      @track_id = @json["message"]["body"]["track_list"][0]["track"]["track_id"]
-      @lyrics = MusixmatchSetter.new.lyrics
+      @track = MusixmatchSetter.new.get_track
+      @track_id = @track["message"]["body"]["track_list"][0]["track"]["track_id"]
+      @lyrics = MusixmatchSetter.new.get_lyrics
       @subtitles = @lyrics["message"]["body"]["subtitle_list"][0]["subtitle"]["subtitle_body"]
     end
   end
--- lib/musixmatch.rb
+++ lib/musixmatch.rb
@@ -3,14 +3,14 @@ require 'json'
 
 class MusixmatchSetter
 
-  def response
+  def get_track
     url = "http://api.musixmatch.com/ws/1.1/track.search?apikey=1ded3ade3e63977aef9212b43320afb1&q_track=back%20to%20december&q_artist=taylor%20swift&f_has_lyrics=1"
     # url = "http://api.musixmatch.com/ws/1.1/#{method}?apikey=#{ENV['MUSIXMATCH_API_KEY']}&q_track=#{track}&q_artist=#{artist}&f_has_lyrics=1"
     # this method will need to take in an argument (method, _____?, track, artist)
     JSON.parse(HTTParty.get(url))
   end
 
-  def lyrics
+  def get_lyrics
     url = "http://api.musixmatch.com/ws/1.1/track.subtitles.get?apikey=1ded3ade3e63977aef9212b43320afb1&track_id=9123822"
     JSON.parse(HTTParty.get(url))
   end

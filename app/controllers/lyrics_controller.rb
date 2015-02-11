94b68fbb4b80ba52cb1c0d7c46835abe0a6159f3
Rachel <rmoshier@gmail.com>
2015-02-11 15:55:53 -0800

    finaltime is now an array

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -9,14 +9,15 @@ class LyricsController < ApplicationController
     track = params[:track]
     # subtitles = MusixmatchSetter.new.get_subtitles(track, artist)
     # url = "http://api.musixmatch.com/ws/1.1/#{method}?apikey=#{ENV['MUSIXMATCH_API_KEY']}&q_track=#{track}&q_artist=#{artist}&f_has_lyrics=1"
-    @subtitles_as_hash = MusixmatchSetter.new.subtitles_as_hash(track, artist)
-    respond_to do |format|
-      format.json { render json: @subtitles_as_hash }
-    end
-    @seconds_and_lyrics = MusixmatchSetter.new.hash_to_time(@subtitles_as_hash)
+    @subtitles_as_hash = MusixmatchSetter.subtitles_as_hash(track, artist)
+    @seconds_and_lyrics = MusixmatchSetter.hash_to_time(@subtitles_as_hash)
     respond_to do |format|
       format.json { render json: @seconds_and_lyrics }
     end
+    # @seconds_and_lyrics = MusixmatchSetter.new.hash_to_time(@subtitles_as_hash)
+    # respond_to do |format|
+    #   format.json { render json: @seconds_and_lyrics }
+    # end
     # raise
   end
 
--- lib/musixmatch.rb
+++ lib/musixmatch.rb
@@ -7,21 +7,21 @@ require 'json'
 
 class MusixmatchSetter
 
-  def get_track(track, artist)
+  def self.get_track(track, artist)
     # url = "http://api.musixmatch.com/ws/1.1/track.search?apikey=1ded3ade3e63977aef9212b43320afb1&q_track=back%20to%20december&q_artist=taylor%20swift&f_has_lyrics=1"
     url = "http://api.musixmatch.com/ws/1.1/track.search?"
     # this method will need to take in three argument (method, track, artist)
     JSON.parse(HTTParty.get(url, query: {apikey: ENV['MUSIXMATCH_API_KEY'], q_track: track, q_artist: artist, f_has_lyrics: 1 }))
   end
 
-  def get_lyrics(track_id)
+  def self.get_lyrics(track_id)
     # url = "http://api.musixmatch.com/ws/1.1/track.subtitles.get?apikey=1ded3ade3e63977aef9212b43320afb1&track_id=9123822"
     url = "http://api.musixmatch.com/ws/1.1/track.subtitles.get?apikey=#{ENV['MUSIXMATCH_API_KEY']}&track_id=#{track_id}"
     # this method will need to take in an argument (track_id)
     JSON.parse(HTTParty.get(url))
   end
 
-  def get_subtitles(track, artist)
+  def self.get_subtitles(track, artist)
     @track = get_track(track, artist)
     @track_id = @track["message"]["body"]["track_list"][0]["track"]["track_id"]
     @lyrics = get_lyrics(@track_id)
@@ -29,7 +29,7 @@ class MusixmatchSetter
     return @subtitles
   end
 
-  def subtitles_as_hash(track, artist)
+  def self.subtitles_as_hash(track, artist)
     # raise
     @subtitles = get_subtitles(track, artist)
     chunk = @subtitles.split("\n")
@@ -38,17 +38,20 @@ class MusixmatchSetter
     return obj
   end
 
-  def hash_to_time(obj)
+  def self.hash_to_time(obj)
     newtime = obj.map {|thing| thing[0].gsub(/["\[\]]/, "").split(":") }
     seconds = newtime.map { |time| (time[0].to_i * 60) + time[1].to_f }
     # puts seconds
-    finaltime = {}
+    # finaltime = {}
+    finaltime = []
     lyrics = obj.values
     seconds.each_with_index do |number, index|
-      finaltime[number] = lyrics[index]
+      # finaltime[number] = lyrics[index]
+
+      finaltime << {time: number, lyrics: lyrics[index]}
     end
     return finaltime
-    # raise
+    raise
   end
   # puts finaltime.to_json #returns json
 # ---

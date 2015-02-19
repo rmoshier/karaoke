8adc40971f6963d233014b14a992ca8ba85ddbea
Rachel <rmoshier@gmail.com>
2015-02-18 17:02:52 -0800

    testing musixmatch.rb

--- spec/lib/musixmatch_spec.rb
+++ spec/lib/musixmatch_spec.rb
@@ -2,14 +2,23 @@ require 'spec_helper'
 require 'musixmatch'
 
 describe MusixmatchSetter do
+
+  let(:mms) { MusixmatchSetter.new }
+
+  describe ".get_subtitles" do
+    it "returns a string" do
+      mms.subtitles = "some string"
+      expect(mms.subtitles).to be_an_instance_of String
+    end
+  end
   # it "needs arguments to initialize" do
   #   expect { MusixmatchSetter.new }.to raise_error(ArgumentError)
   # end
 
-  let(:track) { MusixmatchSetter.new.get_track("Circus", "Britney Spears") }
-  it "returns a json" do
-    track.get_track.should be_a json
-  end
+  # let(:track) { MusixmatchSetter.new.get_track("Circus", "Britney Spears") }
+  # it "returns a json" do
+  #   track.get_track.should be_a json
+  # end
 end
 
 # describe "Person" do
